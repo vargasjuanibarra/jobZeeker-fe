2881595575f920dbaf06129d969ca3d6e2a25573
vargasjuanibarra <vargasjuanibarra@gmail.com>
2024-07-12 19:39:20 +0800

    rezise fonts for smaller screen sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Card = ({title, topic, textColor, subtitle, color, button, btnColor, link}) => {
   return (
     <div className={color + " p-4 w-full bg-opacity-70"}>
-        <div className="flex justify-between">
-        <h2 className="text-lg md:text-xl lg:text-2xl italic">{title}<span className={'font-bold not-italic ' + textColor}>{topic}</span></h2>
+        <div className="flex justify-between items-center">
+        <h2 className="text-md md:text-xl lg:text-2xl italic">{title}<span className={'font-bold not-italic ' + textColor}>{topic}</span></h2>
         <Link
         to={link}
-        className={`text-sm md:text-md lg:text-lg inline-block ${btnColor} text-white rounded-lg px-3 py-2 hover:bg-gray-700`}
+        className={`text-xs md:text-md lg:text-lg inline-block ${btnColor} text-white rounded-md px-2 md:px-3 py-2 hover:bg-gray-700`}
         >
         {button}
         </Link>
