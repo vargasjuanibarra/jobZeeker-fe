9d34e45afea93fd27ad492ebf2639e3b66b94e5a
vargasjuanibarra <vargasjuanibarra@gmail.com>
2024-07-18 15:25:09 +0800

    added dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
+
+// Load environment variables from .env file
+config()
+
+const apiUrl = process.env.VITE_API_URL;
+
+if (!apiUrl) {
+  throw new Error('VITE_API_URL is not defined in the environment variables');
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +19,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://jobzeek-be.onrender.com',
+        target: apiUrl,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
